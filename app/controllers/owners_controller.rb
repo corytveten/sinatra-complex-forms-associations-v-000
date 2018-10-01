3a2d698817d97299b11faa00fb836c5e94409c82
Cory Tveten <corytveten@gmail.com>
2018-10-01 17:05:02 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -28,16 +28,12 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do
-    @owner = Owner.find(params[:id])
-    if !params[:owner].keys.include?("pet_ids")
-      params[:owner]["pet_ids"] = []
-    end
+  post '/owners/:id' do
+   	@owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    @owner.save
-    redirect "owners/#{@owner.id}"
+    redirect to "owners/#{@owner.id}"
   end
 end
